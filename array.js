b69e0973b34f6e8a50e4619103385fd04c24501a
Tech-A12 <Tech-A12@Tech-A12s-MacBook-Air.local>
2017-08-31 16:49:11 +0100

    Exercises used as an intro to js

---
+++ array.js
@@ -0,0 +1,77 @@
+/**
+ * Arrays
+ * Most of your answers should be stored in variables called q1, q2 etc.. and the variables printed to the console.
+
+     (i.e) console.log("Question 1" + q1)
+ */
+
+/**
+ * Question 1
+ * Create an array of image sources. Use image1.png, image2.png, and image3.png as the array values.
+ */
+
+// var images = ['image1.png', 'image2.png', 'image3.png'];
+// console.log("Question 1: " , images)
+
+/**
+ * Question 2
+ * Using the array from Question 1, store the first element of the array in variable q2.
+ */
+
+// var q2 = image[0];
+//console.log("Question 2 " , q2);
+
+/**
+ * Question 3
+ * Get the length of the first array (number of elements in the array) and store it in variable q3
+ */
+
+// var q3 = images.length
+// console.log("Question 3: ", q3);
+
+/**
+ * Question 4
+ *  Using the array from Question 1, store the last element of the array in variable q4. Hint: How can we get the number of elements in the array?
+ */
+
+
+// var q4 = images[images.length -1];
+// console.log("Question 4 " , q4);
+
+
+// ____________________________________________________________________________
+
+/**
+ * Arrays + Iteration
+ * Most of your answers should be stored in variables called q1, q2 etc.. and the variables printed to the console.
+
+     (i.e) console.log("Question 1" + q1)
+ */
+
+/**
+ * Question 1
+ * Create an array of numbers using 1,2,3, and 4 as values. Use forEach to increase each value by 1 and store the value back in the array.
+The end result should be an array of numbers with values 2,3,4,5
+ */
+
+// var numbers = [1, 2, 3, 4];
+// numbers.forEach(function (el, i) {
+// numbers[i] = el + 1;
+// console.log(++el)
+// } )
+
+
+/**
+ * Question 2
+ * Using the array from Question 1, find the average of the numbers in the array (average=sum of all numbers/number of numbers). Store the average in q2.
+ */
+
+
+// var i = 0;
+// for (var i = 0; i < numbers.length; i++) {
+	// q2 += numbers[i];
+// }
+ // q2 = q2/numbers.length;
+
+
+
---
+++ main.js
@@ -0,0 +1,79 @@
+// Shows a popup bo on the screen with some text inside it.
+// alert('Some information');
+
+// output some text to the chrome console
+// console.log('Some other information');
+
+// returns us the datatype of a variable
+// typeof()
+
+// int
+	// 0, 3, 6, -1, 123, 87
+
+// float
+	// -0.1, 1.1, 45.98
+
+//  Advanced Math Operator
+	// Math.pow(2,4)
+	// Math.sqrt(12)
+	// Math.random()
+	// Math.floor(2.2)
+	// Math.ceil(2.2)
+
+
+// **** Strings ****
+
+	// .length
+	// .charAt()
+	// .replace('','')
+	// .toUpperCase()
+	// parseInt("12.332")
+	// parseFloat("12.332")
+
+
+// **** Array ****
+
+	// .pop()
+	// .push('jello')
+	// .shift()
+	// .unshift('jello')
+
+
+
+var daysOfTheWeek = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
+
+//  for loop
+
+// for (var i = 0; i < daysOfTheWeek.length; i++) {
+// 	var current = daysOfTheWeek[i]
+// 	console.log(current.toUpperCase())
+
+// }
+
+daysOfTheWeek.forEach(function (el, i) {
+	console.log(el)
+	console.log(i)
+} )
+
+// daysOfTheWeek.every()
+// daysOfTheWeek.some()
+// daysOfTheWeek
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
